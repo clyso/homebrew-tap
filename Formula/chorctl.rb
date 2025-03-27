9f28f78497fce0315df8b7f0bd3d68241ae12772
clyso_bot <release-bot@clyso.com>
2025-03-27 11:38:55 +0100

    Brew formula update for chorctl version v0.5.14

--- Formula/chorctl.rb
+++ Formula/chorctl.rb
@@ -5,21 +5,21 @@
 class Chorctl < Formula
   desc "Management CLI for chorus service"
   homepage "https://github.com/clyso/chorus"
-  version "0.5.13"
+  version "0.5.14"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/clyso/chorus/releases/download/v0.5.13/chorctl_v0.5.13_darwin_amd64.tar.gz"
-      sha256 "23f619f4e88bb0b11771793601a996ecf7993de2fdafc3338016bd2a36c8a2bd"
+      url "https://github.com/clyso/chorus/releases/download/v0.5.14/chorctl_v0.5.14_darwin_amd64.tar.gz"
+      sha256 "a5f549f780d7c39b27500ab53dbcf0e246aa365abcd317305d2a361b367c60a5"
 
       def install
         bin.install "chorctl"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/clyso/chorus/releases/download/v0.5.13/chorctl_v0.5.13_darwin_arm64.tar.gz"
-      sha256 "998b75e1499b1af306673661c55489a07777c44c813eb56331bd2bde425d9f3d"
+      url "https://github.com/clyso/chorus/releases/download/v0.5.14/chorctl_v0.5.14_darwin_arm64.tar.gz"
+      sha256 "3b20f70fbd58d3660738995de4b1612793e18d569a35957cdb0d73e49a932496"
 
       def install
         bin.install "chorctl"
@@ -29,16 +29,16 @@ class Chorctl < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/clyso/chorus/releases/download/v0.5.13/chorctl_v0.5.13_linux_amd64.tar.gz"
-      sha256 "109c96a210663fd51e4bc0079e18c53eeec0b7b1fdc9b1e3102767fe43d7eaf1"
+      url "https://github.com/clyso/chorus/releases/download/v0.5.14/chorctl_v0.5.14_linux_amd64.tar.gz"
+      sha256 "b9419feb6025b707f60f15ca9ac1556e0ae366748acb771dfc13efef824c6eff"
 
       def install
         bin.install "chorctl"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/clyso/chorus/releases/download/v0.5.13/chorctl_v0.5.13_linux_arm64.tar.gz"
-      sha256 "db2a6a2a15973e73a449409e819d7b6226dfedc6e3695afc7ead443e750ff47c"
+      url "https://github.com/clyso/chorus/releases/download/v0.5.14/chorctl_v0.5.14_linux_arm64.tar.gz"
+      sha256 "1fc776df34c8c1733a8c6944c8a47b52a7bfe628169f5983d783e4db6ff07944"
 
       def install
         bin.install "chorctl"
